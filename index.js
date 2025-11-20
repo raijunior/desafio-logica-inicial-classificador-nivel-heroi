0de06ed4418e039fddf7883a31cc003d50c1c394
raijunior <junior-oliveira3012@hotmail.com>
2025-11-20 15:30:29 -0300

    Desafio classificador de nível de herói finalizado

---
+++ index.js
@@ -0,0 +1,30 @@
+//Variavel nome do Herói
+let nomeDoHeroi = "Raijunior";
+
+//Quantidade de experiência (XP) do herói
+let xpDoHeroi = 7002;
+
+//Classificação do nível de Herói por XP
+
+let nivel;
+
+if (xpDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
+    nivel = "Imortal";
+// Maior ou igual a 10001
+} else { 
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`);
